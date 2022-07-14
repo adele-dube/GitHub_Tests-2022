bbfe433b223ac543c8e054e7932508756c7c6ade
Adele_D <elmaadeledube@gmail.com>
2022-07-14 14:57:13 +0300

    Edited <title> and <h1> in index.html and added JS to <h1>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-alert("Welcome!");
+let greeting = alert("Welcome!");
+
+let customizedName = prompt("What is your name?");
+
+let customizedH1 = document.querySelector("h1");
+customizedH1.innerHTML = `Hello ${customizedName}!`;
